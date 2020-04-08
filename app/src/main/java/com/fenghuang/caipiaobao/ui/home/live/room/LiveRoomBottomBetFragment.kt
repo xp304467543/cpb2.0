7b34a7e7be7621cac81600cab82fded161813612
qintian <304467543@qq.com>
2020-04-08 17:38:24 +0800

    竞猜页面

--- app/src/main/java/com/fenghuang/caipiaobao/ui/home/live/room/LiveRoomBottomBetFragment.kt
+++ app/src/main/java/com/fenghuang/caipiaobao/ui/home/live/room/LiveRoomBottomBetFragment.kt
@@ -1,11 +1,11 @@
 package com.fenghuang.caipiaobao.ui.home.live.room
-
+import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.ImageView
-import android.widget.TextView
+import android.widget.Spinner
 import com.fenghuang.caipiaobao.R
+import com.fenghuang.caipiaobao.ui.lottery.data.LotteryApi
 import com.fenghuang.caipiaobao.widget.dialog.bottom.BaseBottomSheetFragment
-
-
 /**
  *
  * @ Author  QinTian
@@ -16,17 +16,35 @@ import com.fenghuang.caipiaobao.widget.dialog.bottom.BaseBottomSheetFragment
 
 class LiveRoomBottomBetFragment(override val layoutResId: Int = R.layout.dialog_live_bet) : BaseBottomSheetFragment() {
 
+    var spanner: Spinner? = null
 
     override fun initView() {
         rootView?.findViewById<ImageView>(R.id.imgBetCLose)?.setOnClickListener {
             dismiss()
         }
+        rootView?.findViewById<View>(R.id.tvLiveBetClose)?.setOnClickListener {
+            dismiss()
+        }
+        spanner = rootView?.findViewById(R.id.spLotteryType)
     }
 
 
     override fun initData() {
-
+        val type = LotteryApi.getLotteryType()
+        type.onSuccess {
+            val title = arrayListOf<String>()
+            for (data in it) {
+                title.add(data.cname)
+            }
+          if (!title.isNullOrEmpty())  initSpanner(title)
+        }
     }
 
+    private fun initSpanner(data: List<String>) {
+        val spAdapter = mContext?.let { ArrayAdapter<String>(it, android.R.layout.simple_spinner_item, data) }
+        spAdapter?.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spanner?.adapter = spAdapter
+
+    }
 
 }
