3f3ff501a772534f3b0032b338b2f72cdf6db361
james <1515872591@qq.com>
2020-04-10 18:01:19 +0800

    新建彩种选择框dialog

--- app/src/main/java/com/fenghuang/caipiaobao/ui/home/live/room/LiveRoomBottomBetFragment.kt
+++ app/src/main/java/com/fenghuang/caipiaobao/ui/home/live/room/LiveRoomBottomBetFragment.kt
@@ -1,11 +1,15 @@
 package com.fenghuang.caipiaobao.ui.home.live.room
+
 import android.view.View
-import android.widget.ArrayAdapter
 import android.widget.ImageView
-import android.widget.Spinner
+import android.widget.TextView
 import com.fenghuang.caipiaobao.R
 import com.fenghuang.caipiaobao.ui.lottery.data.LotteryApi
 import com.fenghuang.caipiaobao.widget.dialog.bottom.BaseBottomSheetFragment
+import com.fenghuang.caipiaobao.widget.dialog.bottom.BottomLottertSeletDialog
+import kotlinx.android.synthetic.main.dialog_lottery_select.*
+import kotlinx.android.synthetic.main.dialog_wheel_view.tvWheelSure
+
 /**
  *
  * @ Author  QinTian
@@ -16,7 +20,8 @@ import com.fenghuang.caipiaobao.widget.dialog.bottom.BaseBottomSheetFragment
 
 class LiveRoomBottomBetFragment(override val layoutResId: Int = R.layout.dialog_live_bet) : BaseBottomSheetFragment() {
 
-    var spanner: Spinner? = null
+    private var opt1SelectedPosition: Int = 0
+    private var tvLotterySelectType: TextView? = null
 
     override fun initView() {
         rootView?.findViewById<ImageView>(R.id.imgBetCLose)?.setOnClickListener {
@@ -25,7 +30,7 @@ class LiveRoomBottomBetFragment(override val layoutResId: Int = R.layout.dialog_
         rootView?.findViewById<View>(R.id.tvLiveBetClose)?.setOnClickListener {
             dismiss()
         }
-        spanner = rootView?.findViewById(R.id.spLotteryType)
+        tvLotterySelectType = rootView?.findViewById(R.id.tvLotterySelectType)
     }
 
 
@@ -36,15 +41,28 @@ class LiveRoomBottomBetFragment(override val layoutResId: Int = R.layout.dialog_
             for (data in it) {
                 title.add(data.cname)
             }
-          if (!title.isNullOrEmpty())  initSpanner(title)
+            if (!title.isNullOrEmpty()) initDialog(title)
         }
     }
 
-    private fun initSpanner(data: List<String>) {
-        val spAdapter = mContext?.let { ArrayAdapter<String>(it, android.R.layout.simple_spinner_item, data) }
-        spAdapter?.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spanner?.adapter = spAdapter
-
+    private fun initDialog(title: ArrayList<String>) {
+        tvLotterySelectType?.setOnClickListener {
+            mContext?.let {
+                val lortterySelectDialog = BottomLottertSeletDialog(it, title)
+                tvLotterySelectType?.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.mipmap.select_top, 0)
+                lortterySelectDialog.setOnDismissListener {
+                    tvLotterySelectType?.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.mipmap.select_bottom, 0)
+                }
+                lortterySelectDialog.tvWheelSure.setOnClickListener {
+                    tvLotterySelectType?.text = lortterySelectDialog.lotteryPickerView.opt1SelectedData as String
+                    opt1SelectedPosition = lortterySelectDialog.lotteryPickerView.opt1SelectedPosition
+                    lortterySelectDialog.dismiss()
+                }
+                lortterySelectDialog.lotteryPickerView.opt1SelectedPosition = opt1SelectedPosition
+                lortterySelectDialog.show()
+            }
+        }
     }
+
 
 }
---
+++ app/src/main/java/com/fenghuang/caipiaobao/widget/dialog/bottom/BottomLottertSeletDialog.kt
@@ -0,0 +1,49 @@
+package com.fenghuang.caipiaobao.widget.dialog.bottom
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.Gravity
+import android.view.ViewGroup
+import com.fenghuang.baselib.utils.ViewUtils
+import com.fenghuang.caipiaobao.R
+import com.zyyoona7.picker.OptionsPickerView
+import kotlinx.android.synthetic.main.dialog_lottery_select.*
+
+/**
+ *
+ * @ Author  danniel
+ * @ Describe 彩种选择器
+ *
+ */
+
+class BottomLottertSeletDialog(context: Context, val title: ArrayList<String>) : Dialog(context) {
+
+
+
+    init {
+        setContentView(R.layout.dialog_lottery_select)
+        window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        window!!.setGravity(Gravity.BOTTOM)
+        val lp = window!!.attributes
+        lp.width = ViewGroup.LayoutParams.MATCH_PARENT // 宽度
+        lp.dimAmount = 0.3f
+        window!!.attributes = lp
+
+        val mOptionsPickerView = findViewById<OptionsPickerView<String>>(R.id.lotteryPickerView)
+        mOptionsPickerView.setData(title)
+        mOptionsPickerView.setTextSize(18f, true)
+        mOptionsPickerView.setShowDivider(true)
+        mOptionsPickerView.setVisibleItems(6)
+        mOptionsPickerView.setDividerColor(ViewUtils.getColor(R.color.grey_dd))
+        mOptionsPickerView.setLineSpacing(80f)
+        mOptionsPickerView.setSelectedItemTextColor(ViewUtils.getColor(R.color.black))
+        mOptionsPickerView.setNormalItemTextColor(ViewUtils.getColor(R.color.grey_e6))
+        tvWheelCancel.setOnClickListener {
+            dismiss()
+        }
+    }
+
+
+}
